ab4b9f576eeb32a2e26c5a34669943be0bd07017
ph7dev <ph7dev@gmail.com>
2016-04-03 20:40:17 +0300

    task #23-28 completed; task #13 refactoring

--- arrays_loops_tasks/2.php
+++ arrays_loops_tasks/2.php
@@ -137,15 +137,12 @@ echo "Число 1000 поделено на 2: $num раз.";
 //13
 echo tit(13);
 
-function block ($arg1){
-    for ($i = 1; $i <= 9; $i++){
-        $sum = $arg1*$i;
-        echo $arg1."x".$i." = $sum<br>";
-    }
-}
-
 for ($i = 1; $i <= 9; $i++){
-    echo block($i)."<br>";
+    for ($x = 1; $x <= 9; $x++){
+        $sum = $i*$x;
+        echo $i."x".$x." = $sum".B;
+    }
+    echo B;
 }
 
 //14
@@ -237,7 +234,7 @@ for ($i = 0; $i < 20; $i++){
 echo tit(21);
 
 $str21 = '';
-for ($i = 1; $i < 12; $i++){
+for ($i = 1; $i < 10; $i++){
     for ($g = 0; $g < $i; $g++){
         $str21 .= "$i";
     }
@@ -257,16 +254,144 @@ while ($cnt == true){
     $cnt--;
 }
 
+//23
+echo tit(23);
+
+$number = 123;
+$number = (string)$number;
+
+for ($i = 0; $i <= strlen($number)-1; $i++){
+    $arr23[] = $number[$i];
+}
+
+echo "Сумма елементов числа ".$number." = ".array_sum($arr23).B;
+
+//24
+echo tit(24);
+$init_num = 7374377;
+$search_num = 3;
+
+$init_num = (string)$init_num;
+
+for ($i = 0; $i <= strlen($init_num)-1; $i++){
+    $arr23[] = $init_num[$i];
+}
+
+$res = array_keys($arr23, "$search_num");
+
+echo "Число ".$search_num." встречается в числе ".$init_num." - ".count($res)." раз(а).";
 
 
+//25
+echo tit(25);
+
+for ($i = 0; $i <= 5; $i++){
+    $rand_mass[] = rand(1,100);
+}
+
+function my($a, $b){
+    if ($a == $b){
+        return 0;
+    }
+    return ($a < $b) ? -1 : 1;
+}
+
+usort($rand_mass, "my");
+$max = count($rand_mass)-1;
+
+echo "<pre>";
+print_r($rand_mass);
+echo "</pre>";
+
+echo "Минимальное значение = {$rand_mass[0]}".B;
+echo "максимальное значение = {$rand_mass["$max"]}".B;
+
+$min = $rand_mass[0];
+$rand_mass[0] = $rand_mass["$max"];
+$rand_mass["$max"] = $min;
+
+echo "<pre>";
+print_r($rand_mass);
+echo "</pre>";
 
 
+//26
+echo tit(26);
+
+//создать и заполнить массив
+for ($i = 0; $i <= 6; $i++){
+    $rand_mass26[] = rand(0,5);
+}
+
+//создаем пустой массив для хранения значений у которых буду парные ключи
+//и значения не равны нулю
+$two = array();
+echo "исходный/начальный массив:".B;
+echo "<pre>";
+print_r($rand_mass26);
+echo "</pre>";
+
+foreach ($rand_mass26 as $key=>$value){
+    if (!($key%2) && $value != 0){
+
+        //в массив $two записываем все значения у которых парный индекс и значения не равны 0
+        //заодно сохраняем исходные ключи значений.
+        $two["$key"] = $value;
+
+        //из исходного массива удаляем значения и ключи которые были записаны в массив $two.
+        unset($rand_mass26["$key"]);
+    }elseif(!($key%2) && $value == 0){
+
+        //так же если ключ парный и значение равно нулю -
+        //тоже удаляем его из массива, так как по условию такие значения нам вообще не нужны,
+        //мы не будем их использовать
+        unset($rand_mass26["$key"]);
+    }
+}
+
+//выводим элементы у которых непарый индекс и значения больше нуля
+echo "элементы которые больше нуля и которых <span style='color: red;'><b>НЕ</b></span>парный индекс:".B;
+foreach($rand_mass26 as $k=>$item){
+    if ($item > 0){
+        echo "[$k] : $item".B;
+    }
+}
+$res = 1;
+foreach($two as $k=>$v){
+    $res *= $v;
+}
+/*
+//раскомментировать для просмотра элементов которые перемножаются
+echo "<pre>";
+echo "two".B;
+print_r($two);
+echo "</pre>";
+*/
+echo B."Результат произведения значений которые больше нуля и у которых парный индекс: сумма = ".$res.B;
 
 
+//27
+echo tit(27);
 
+$cols = rand (1,10);//столбцы
+$rows = rand (1,10);//строки
+$colors = array('red','yellow','blue','gray','maroon','brown','green');
 
+?>
+<table>
+<?php
 
-
+for ($i = 0; $i <= $rows; $i++){
+    echo "<tr>";
+        for ($x = 0; $x <= $cols; $x++){
+            echo "<td style='background:".$colors[rand(0,6)]."'>";
+            echo "<b style='color:white'>".rand(99,99999)."</b>";
+            echo "</td>";
+        }
+    echo "</tr>";
+}
+?>
+</table>
 
 
 
